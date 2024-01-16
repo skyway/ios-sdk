01b4bd96fbd322df7a17f23acae2cdcff5653a27
kei <kei.muranaka@ntt.com>
2024-01-16 16:50:28 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,15 +24,15 @@ let package = Package(
         .binaryTarget(
             name: "SkyWayRoom",
             url: "https://github.com/skyway/ios-sdk/releases/download/1.8.1/SkyWayRoom.xcframework.zip",
-            checksum: "10d5b10836b4fe63bece672637847a4c8baa95d566ea29ecab6e88944a6799b0"),
+            checksum: "ed5b4e27e179af6b1e48ded644588d0d4bccd529fb095de6fe595b419d73e037"),
         .binaryTarget(
             name: "SkyWayCore",
             url: "https://github.com/skyway/ios-sdk/releases/download/1.8.1/SkyWayCore.xcframework.zip",
-            checksum: "9b198e406fe198ef5052ac9296c2aecd180f48e4b628e87b966cb0106ce99969"),
+            checksum: "0f6ca9f3f7bdf93172d7dfb8f67093804404614b850ab83a6dbd86e4e11afd1c"),
         .binaryTarget(
             name: "SkyWaySFUBot",
             url: "https://github.com/skyway/ios-sdk/releases/download/1.8.1/SkyWaySFUBot.xcframework.zip",
-            checksum: "cd237d3c5e3b0df52120d691130b9117d8ade1ff3cebf60812588dcdc9a00853"),
+            checksum: "8487f82cfcbfb4d9708c5cbab01366ec1bb10c99f33be5d93e4d1758ca71b889"),
         .binaryTarget(
             name: "WebRTC",
             url: "https://github.com/skyway/skyway-ios-webrtc-specs/releases/download/104.0.1/WebRTC.xcframework.zip",
